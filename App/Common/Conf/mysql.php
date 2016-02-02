0201afa71121ccbfe938c4c65215339326d54614
unknown <>
2016-02-02 20:36:56 +0800

    change database config

--- App/Common/Conf/mysql.php
+++ App/Common/Conf/mysql.php
@@ -2,10 +2,15 @@
 return array(
 	// 数据库配置项	
 	'DB_TYPE'   => 'mysql',		// 数据库类型
-	'DB_HOST'   => 'localhost', // 服务器地址
-	'DB_NAME'   => 'DomainSEO',	// 数据库名
-	'DB_USER'   => 'root',		// 用户名
-	'DB_PWD'    => 'root',			// 密码
-	'DB_PORT'   => 3306,		// 端口
+	'DB_HOST'   => SAE_MYSQL_HOST_M, // 服务器地址
+	'DB_NAME'   => SAE_MYSQL_DB,	// 数据库名
+	'DB_USER'   => SAE_MYSQL_USER,		// 用户名
+	'DB_PWD'    => SAE_MYSQL_PASS,			// 密码
+	'DB_PORT'   => SAE_MYSQL_PORT ,		// 端口
+//	'DB_HOST'   => 'localhost', // 服务器地址
+//	'DB_NAME'   => 'DomainSEO',	// 数据库名
+//	'DB_USER'   => 'root',		// 用户名
+//	'DB_PWD'    => 'root',			// 密码
+//	'DB_PORT'   => 3306,		// 端口
 	'DB_PREFIX' => 'm9_',	    // 数据库表前缀
 );
