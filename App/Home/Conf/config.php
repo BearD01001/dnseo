13d174724644d9d8225306e90ce906e80c8a9a1a
Dino <heyke2114@126.com>
2016-02-06 18:38:06 +0800

    update all files

--- App/Home/Conf/config.php
+++ App/Home/Conf/config.php
@@ -1,11 +1,10 @@
 <?php
 return array(
     'URL_MODEL' => 2,  //普通模式 0 ,PATHINFO模式 1 ,REWRITE模式 2 ,兼容模式 3
-    'OUTPUT_ENCODE' =>true,
-    'URL_ROUTER_ON' => true, // url路由开关
-    'URL_ROUTE_RULES' => array(
-        '/^www\.(.*)$/i' => ':1',
-        '/^([a-z0-9][-a-z0-9]{0,62}(\.[a-z0-9][-a-z0-9]{0,62})+)$/i' => 'index.php/Domain/detail/domain/:1',
-        '/^(\d+)$/i' => 'index.php/Index/index/cat/:1',
-    ),
+//    'URL_ROUTER_ON' => true, // url路由开关
+//    'URL_ROUTE_RULES' => array(
+//        '/^www\.(.+\..+)$/i' => '/:1',
+//        '/([-a-z0-9]+\.[-a-z0-9]+)$/i' => '/Domain/detail/domain/:1',
+//        '/^(\d+)$/i' => '/Index/index/cat/:1',
+//    ),
 );
