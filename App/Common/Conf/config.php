9a3eba159a94c19d95b9ed643a47ebcca652cd87
Dino <heyke2114@126.com>
2016-02-16 19:50:30 +0800

    bugs fixed

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -13,5 +13,5 @@ return array(
     'URL_MODEL' => 1,  //普通模式 0 ,PATHINFO模式 1 ,REWRITE模式 2 ,兼容模式 3
     'URL_HTML_SUFFIX' => 'html',
     /*载入数据库配置文件*/
-    'LOAD_EXT_CONFIG' => 'mysql'
+//    'LOAD_EXT_CONFIG' => 'mysql'
 );
