d81fcea38bb254851e390659e5ddc143adcc5933
Dino <heyke2114@126.com>
2016-03-07 09:37:25 +0800

    modified public path in the sae cloud

--- App/Common/Conf/config_sae.php
+++ App/Common/Conf/config_sae.php
@@ -8,4 +8,8 @@
 
 return array(
 //    SAE参数配置文件
+//    重新配置公共文件路径，多域加速文件加载，同时使用SAE域名可减少流量消耗
+    'TMPL_PARSE_STRING' => array(
+        '__PUBLIC__' => 'http://dnseo.applinzi.com/Public'
+    )
 );
