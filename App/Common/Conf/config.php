709ec41585c389c87cbb3bd6922e36b5ba14693d
Dino <heyke2114@126.com>
2016-03-07 11:12:16 +0800

    end

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -10,7 +10,7 @@ return array(
 	'SHOW_PAGE_TRACE' => false, // 输出页面Trace信息
     'SHOW_ERROR_MSG' => true, // 显示报错信息
 	'TMPL_FILE_DEPR' => '_',
-    'URL_CASE_INSENSITIVE' => true,  //忽略URL大小写，Linux必备
+    'URL_CASE_INSENSITIVE' => false,  //忽略URL大小写，Linux必备
     'URL_MODEL' => 1,  //普通模式 0 ,PATHINFO模式 1 ,REWRITE模式 2 ,兼容模式 3
     'URL_HTML_SUFFIX' => 'html',
     /*载入数据库配置文件*/
